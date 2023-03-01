92951ff24194b8fde7586b21a27ba9c1762aa40c
Daniel Weninger <dweninger315@gmail.com>
2023-02-28 23:10:45 -0600

    removed old policy_compiler dir

--- policy_compiler/interpreter/policy-interpreter.h
+++
@@ -1,9 +0,0 @@
-
-void assignLevelToFile(char * assignee, char * assignmentData, char * pathToFiles);
-void assignLabelToFile(char * assignee, char * assignmentData, char * pathToFiles);
-int labelExistsInXAttrs(char * checkLabel, char * fileJson);
-int fileAlreadyAccessed(char * file);
-void writeUserLevelToDB(char * assignee, char * assignmentData);
-void writeUserLabelToDB(char * assignee, char * assignmentData);
-void setxattrErrorPrints();
-void getxattrErrorPrints();
