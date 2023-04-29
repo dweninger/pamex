87db84219387ea9a4adaf03c22bfb239d1979bc4
Daniel Weninger <dweninger315@gmail.com>
2023-04-29 17:15:30 -0500

    rebase and stash

--- pamx/interpreter/policy-interpreter.h
+++
@@ -1,9 +0,0 @@
-
-void assign_level_to_file(char * assignee, char * assignment_data, char * path_to_files);
-void assign_label_to_file(char * assignee, char * assignment_data, char * path_to_files);
-int label_exists_in_xattrs(char * check_label, char * file_json);
-int file_already_accessed(char * file);
-void write_user_level_to_db(char * assignee, char * assignment_data, char * user_db_out_path);
-void write_user_label_to_db(char * assignee, char * assignment_data, char * user_db_out_path);
-void setxattr_error_prints();
-void getxattr_error_prints();
